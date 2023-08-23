72cd7b8853165e00ccd368ace1c0a7be9573cd4a
JuanAnVillanueva <101562815+JuanAnVillanueva@users.noreply.github.com>
2023-08-23 14:06:52 -0300

    Update Remera.java

--- POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/Remera.java
+++ POO_SHOP_ON_LINE/src/main/java/com/mycompany/poo_shop_on_line/Remera.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.poo_shop_on_line;
 
-/**
- *
- * @author CS09
- */
+
 public class Remera extends Producto {
     private String talle;
     
